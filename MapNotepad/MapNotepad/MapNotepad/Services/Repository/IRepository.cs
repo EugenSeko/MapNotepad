943ef31a58208fccf90b7ed7f051aa955d2a25db
DESKTOP-11L4M4B\Intern_03 <satyagrah108@gmail.com>
2021-11-01 15:57:08 +0000

    Added sqlite-net-pcl and create the repository

---
+++ MapNotepad/MapNotepad/MapNotepad/Services/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using MapNotepad.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapNotepad.Services.Repository
+{
+   public interface IRepository
+    {
+        Task<int> InsertAsync<T>(T entity) where T : IEntityBase, new();
+        Task<int> UpdateAsync<T>(T entity) where T : IEntityBase, new();
+        Task<int> DeleteAsync<T>(T entity) where T : IEntityBase, new();
+        Task<List<T>> GetAllAsync<T>() where T : IEntityBase, new();
+        Task<int> DeleteAllAsync<T>() where T : IEntityBase, new();
+    }
+}
---
+++ MapNotepad/MapNotepad/MapNotepad/Services/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using MapNotepad.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapNotepad.Services.Repository
+{
+   public class Repository : IRepository
+    {
+        private Lazy<SQLiteAsyncConnection> _database;
+
+        public Repository()
+        {
+            _database = new Lazy<SQLiteAsyncConnection>(() =>
+            {
+                var path = Path.Combine(Environment.GetFolderPath(Environment
+                                         .SpecialFolder.LocalApplicationData), "contacts.bd");
+                var database = new SQLiteAsyncConnection(path);
+                
+                return database;
+            });
+        }
+
+
+        #region --- Common Methods ---
+        public Task<int> DeleteAsync<T>(T entity) where T : IEntityBase, new()
+        {
+            return _database.Value.DeleteAsync(entity);
+        }
+        public Task<List<T>> GetAllAsync<T>() where T : IEntityBase, new()
+        {
+            return _database.Value.Table<T>().ToListAsync();
+        }
+        public Task<int> InsertAsync<T>(T entity) where T : IEntityBase, new()
+        {
+            return _database.Value.InsertAsync(entity);
+        }
+        public Task<int> UpdateAsync<T>(T entity) where T : IEntityBase, new()
+        {
+            return _database.Value.UpdateAsync(entity);
+        }
+        public Task<int> DeleteAllAsync<T>() where T : IEntityBase, new()
+        {
+            return _database.Value.DeleteAllAsync<T>();
+        }
+        #endregion
+    }
+}
