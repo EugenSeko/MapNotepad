6336dccf77b7ff1ed293f8fe37e16dc7188a9ffe
DESKTOP-11L4M4B\Intern_03 <satyagrah108@gmail.com>
2021-11-02 16:21:29 +0000

    Added helpers clases

---
+++ MapNotepad/MapNotepad/MapNotepad/ViewModel/BaseViewModel.cs
@@ -0,0 +1,50 @@
+﻿using MapNotepad.View;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapNotepad.ViewModel
+{
+
+    class BaseViewModel : BindableBase
+    {
+        private readonly INavigationService _navigationService;
+        public BaseViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+        #region --- Navigation ---
+        public async void NavigateGoBack()
+        {
+            await _navigationService.GoBackAsync();
+        }
+
+        public async void GoToMainPage()
+        {
+            await _navigationService.NavigateAsync("/" + nameof(MainPage));
+        }
+        public async void GoBackToRootAsync()
+        {
+            await _navigationService.GoBackToRootAsync();
+        }
+        public async void GoToLoginPage()
+        {
+            await _navigationService.NavigateAsync(nameof(LoginPage));
+        }
+        public async void GoToRegisterPage()
+        {
+            await _navigationService.NavigateAsync("/" + nameof(RegisterPage));
+        }
+        public async void GoToRegisterAndPasswordPage()
+        {
+            await _navigationService.NavigateAsync(nameof(RegisterAndPassword));
+        }
+        public async void GoToLoginAndRegisterPage()
+        {
+            await _navigationService.NavigateAsync(nameof(LoginAndRegisterPage));
+        }
+        #endregion
+    }
+}
