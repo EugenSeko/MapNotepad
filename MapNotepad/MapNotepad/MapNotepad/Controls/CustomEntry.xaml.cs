808458273c990a82ca6b914ac32f7caa983aeedf
EugenSeko <satyagrah108@gmail.com>
2021-11-25 17:17:15 +0200

    fixed display of a cross in the input field

--- MapNotepad/MapNotepad/MapNotepad/Controls/CustomEntry.xaml.cs
+++ MapNotepad/MapNotepad/MapNotepad/Controls/CustomEntry.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows.Input;
 using MapNotepad.Helpers;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace MapNotepad.Controls
 {
@@ -16,7 +11,6 @@ namespace MapNotepad.Controls
         {
             InitializeComponent();
         }
-
 
         #region -- Public properties --
         public static readonly BindableProperty OnFocusedEventCommandProperty =
@@ -33,7 +27,6 @@ namespace MapNotepad.Controls
             get { return (ICommand)GetValue(OnUnFocusedEventCommandProperty); }
             set { SetValue(OnUnFocusedEventCommandProperty, value); }
         }
-
         public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create(
             propertyName: nameof(MaxLength),
             returnType: typeof(int),
@@ -45,180 +38,149 @@ namespace MapNotepad.Controls
             set => SetValue(MaxLengthProperty, value);
             get => (int)GetValue(MaxLengthProperty);
         }
-
         public static readonly BindableProperty TextProperty = BindableProperty.Create(
             propertyName: nameof(Text),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string Text
         {
             set => SetValue(TextProperty, value);
             get => (string)GetValue(TextProperty);
         }
-
         public static readonly BindableProperty TextColorProperty = BindableProperty.Create(
             propertyName: nameof(TextColor),
             returnType: typeof(Color),
             declaringType: typeof(CustomEntry),
             defaultValue: Color.Silver,
             defaultBindingMode: BindingMode.TwoWay);
-
         public Color TextColor
         {
             set => SetValue(TextColorProperty, value);
             get => (Color)GetValue(TextColorProperty);
         }
-
         public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create(
             propertyName: nameof(FontFamily),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string FontFamily
         {
             set => SetValue(FontFamilyProperty, value);
             get => (string)GetValue(FontFamilyProperty);
         }
-
         public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
             propertyName: nameof(Placeholder),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string Placeholder
         {
             set => SetValue(PlaceholderProperty, value);
             get => (string)GetValue(PlaceholderProperty);
         }
-
         public static readonly BindableProperty PlaceholderColorProperty = BindableProperty.Create(
             propertyName: nameof(PlaceholderColor),
             returnType: typeof(Color),
             declaringType: typeof(CustomEntry),
             defaultValue: Color.Silver,
             defaultBindingMode: BindingMode.TwoWay);
-
         public Color PlaceholderColor
         {
             set => SetValue(PlaceholderColorProperty, value);
             get => (Color)GetValue(PlaceholderColorProperty);
         }
-
         public static readonly BindableProperty IsPasswordProperty = BindableProperty.Create(
             propertyName: nameof(IsPassword),
             returnType: typeof(bool),
             declaringType: typeof(CustomEntry),
             defaultValue: false,
             defaultBindingMode: BindingMode.TwoWay);
-
         public bool IsPassword
         {
             set => SetValue(IsPasswordProperty, value);
             get => (bool)GetValue(IsPasswordProperty);
         }
-
         public static readonly BindableProperty IsPasswordHideProperty = BindableProperty.Create(
             propertyName: nameof(IsPasswordHide),
             returnType: typeof(bool),
             declaringType: typeof(CustomEntry),
             defaultValue: false,
             defaultBindingMode: BindingMode.TwoWay);
-
         public bool IsPasswordHide
         {
             set => SetValue(IsPasswordHideProperty, value);
             get => (bool)GetValue(IsPasswordHideProperty);
         }
-
         public static readonly BindableProperty IsButtonVisibleProperty = BindableProperty.Create(
             propertyName: nameof(IsButtonVisible),
             returnType: typeof(bool),
             declaringType: typeof(CustomEntry),
             defaultValue: false,
             defaultBindingMode: BindingMode.TwoWay);
-
         public bool IsButtonVisible
         {
             set => SetValue(IsButtonVisibleProperty, value);
             get => (bool)GetValue(IsButtonVisibleProperty);
         }
-
         public static readonly BindableProperty ClearImageSourceProperty = BindableProperty.Create(
             propertyName: nameof(ClearImageSource),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string ClearImageSource
         {
             set => SetValue(ClearImageSourceProperty, value);
             get => (string)GetValue(ClearImageSourceProperty);
         }
-
         public static readonly BindableProperty EyeOnImageSourceProperty = BindableProperty.Create(
             propertyName: nameof(EyeOnImageSource),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string EyeOnImageSource
         {
             set => SetValue(EyeOnImageSourceProperty, value);
             get => (string)GetValue(EyeOnImageSourceProperty);
         }
-
         public static readonly BindableProperty EyeOffImageSourceProperty = BindableProperty.Create(
             propertyName: nameof(EyeOffImageSource),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string EyeOffImageSource
         {
             set => SetValue(EyeOffImageSourceProperty, value);
             get => (string)GetValue(EyeOffImageSourceProperty);
         }
-
         public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(
             propertyName: nameof(ImageSource),
             returnType: typeof(string),
             declaringType: typeof(CustomEntry),
             defaultValue: string.Empty,
             defaultBindingMode: BindingMode.TwoWay);
-
         public string ImageSource
         {
             set => SetValue(ImageSourceProperty, value);
             get => (string)GetValue(ImageSourceProperty);
         }
-
         private ICommand _buttonCommand;
         public ICommand ButtonCommand => _buttonCommand ??= SingleExecutionCommand.FromFunc(OnButtonCommandAsync);
-
         private ICommand _focusedCommand;
         public ICommand FocusedCommand => _focusedCommand ??= SingleExecutionCommand.FromFunc(OnFocusedCommandAsync);
-
         private ICommand _unfocusedCommand;
         public ICommand UnFocusedCommand => _unfocusedCommand ??= SingleExecutionCommand.FromFunc(OnUnfocusedCommandAsync);
-
         #endregion
-
         #region -- Overrides --
-
         protected override void OnPropertyChanged(string propertyName)
         {
             base.OnPropertyChanged(propertyName);
-
             switch (propertyName)
             {
                 case nameof(IsPassword):
@@ -235,15 +197,11 @@ namespace MapNotepad.Controls
                     {
                         ImageSource = ClearImageSource;
                     }
-
                     break;
             }
         }
-
         #endregion
-
         #region -- Private methods --
-
         private Task OnButtonCommandAsync()
         {
             if (IsPassword)
@@ -266,28 +224,22 @@ namespace MapNotepad.Controls
 
             return Task.CompletedTask;
         }
-
         private Task OnFocusedCommandAsync()
         {
             if (!IsPassword)
             {
                 IsButtonVisible = true;
             }
-
             return Task.CompletedTask;
         }
-
         private Task OnUnfocusedCommandAsync()
         {
             if (!IsPassword)
             {
                 IsButtonVisible = false;
             }
-
             return Task.CompletedTask;
         }
-
         #endregion
-
     }
 }
